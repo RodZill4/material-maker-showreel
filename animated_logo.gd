5bd0bcda88da82ecc522c48a707e0cad15d9388a
Rodz Labs <rodzilla@free.fr>
2025-10-26 05:49:05 +0100

    Added parameters to the showreel scene and started a monthly reel (with materials for oct25)

---
+++ animated_logo.gd
@@ -0,0 +1,4 @@
+extends Node
+
+func end():
+	get_tree().quit()
--- scene.gd
+++ scene.gd
@@ -1,5 +1,16 @@
 extends Node
 
+@export var vertical : bool = false
+
+@export var material_dir : String
+@export var material_descriptions : Dictionary[String, String]
+@export var material_list : Array[String]
+@export var hide_material_names : bool = false
+
+@export var background_color : Color = Color(1.0, 1.0, 1.0)
+@export var text_color : Color = Color(0.0, 0.0, 0.0, 1.0)
+
+
 @onready var circle = $MeshPivot1/MeshPivot2/CirclePivot/Circle
 
 @onready var meshes = [ $MeshPivot1/MeshPivot2/Mesh1, $MeshPivot1/MeshPivot2/Mesh2 ]
@@ -13,9 +24,9 @@ var current_material = 1
 	set(v):
 		angle = v
 		if is_inside_tree():
-			var offset = floor(v)
-			v -= offset
-			$MeshPivot1/MeshPivot2.rotation.z = PI*(offset+lerp(my_smoothstep(v), v, damp))
+			var angle_offset = floor(v)
+			v -= angle_offset
+			$MeshPivot1/MeshPivot2.rotation.z = PI*(angle_offset+lerp(my_smoothstep(v), v, damp))
 
 func my_smoothstep(x : float) -> float:
 	var rv : float = x
@@ -23,74 +34,33 @@ func my_smoothstep(x : float) -> float:
 	rv = smoothstep(0.0, 1.0, rv)
 	rv = smoothstep(0.0, 1.0, rv)
 	rv = smoothstep(0.0, 1.0, rv)
-	rv = smoothstep(0.0, 1.0, rv)
+	#rv = smoothstep(0.0, 1.0, rv)
 	return rv
 
 var shader_time : float = 0.0
 
-var MATERIALS = {
-	ancient_bricks = { name="Ancient bricks", author="DroppedBeat" },
-	ancient_pedestal = { name="Ancient pedestal", author="Tarox" },
-	animated_fire = { name="Animated fire", author="unfa" },
-	animated_radar = { name="Animated radar", author="PixelMuncher" },
-	arcane_compass = { name="Arcane compass", author="wojtekpil" },
-	beach = { name="Beach", author="Tarox" },
-	bookcase = { name="Cozy bookcase", author="Arnklit" },
-	burger = { name="Burger", author="Arnklit" },
-	chained = { name="Chained down", author="Tarox" },
-	chesterfield = { name="Chesterfield", author="TheoDGM" },
-	cliff_rock = { name="Cliff rock", author="Skywolf" },
-	containers = { name="Containers", author="DroppedBeat" },
-	damaged_plaster = { name="Damaged plaster wall", author="Arnklit" },
-	fish_pond = { name="Fish pond", author="DroppedBeat" },
-	floppy_disks = { name="Floppy disks", author="PixelMuncher" },
-	gears_panel = { name="Gears panel", author="RodZilla" },
-	gear_box = { name="Gear box", author="DroppedBeat" },
-	gingerbread = { name="Gingerbread", author="Tarox" },
-	ground_foliage = { name="Ground foliage", author="Arnklit" },
-	hardwood_floor = { name="Decrepit Hardwood floor", author="BurritoLord69" },
-	kryptonite = { name="Kryptonite", author="Tarox" },
-	leather = { name="Stitched leather", author="Tarox" },
-	manhole_cover = { name="Manhole cover", author="PixelMuncher" },
-	matrix_code_rain = { name="Matrix rain code", author="DroppedBeat" },
-	ornamental = { name="Ornamental", author="DroppedBeat" },
-	ornamental_ceiling = { name="Ornamental ceiling", author="Tarox" },
-	polished_turquoise = { name="Polished turquoise", author="LitmusZest" },
-	puddle = { name="Puddle", author="DroppedBeat" },
-	rainy_window = { name="Rainy window", author="unfa" },
-	remnant = { name="Remnant", author="Tarox" },
-	roof_tiles = { name="Old roof tiles", author="Tarox" },
-	rosette = { name="Rosette", author="Tarox" },
-	scarabs_on_hieroglyphs = { name="Scarab Beetles Crawling on Hieroglyphs", author="Arnklit" },
-	sealing_talismans = { name="Sealing Talismans", author="DroppedBeat" },
-	sewn_flesh = { name="Sewn flesh", author="Gin" },
-	smaugs_treasure = { name="Smaug's Treasure", author="Arnklit" },
-	snowman = { name="Snowman", author="Arnklit" },
-	stylized_flowing_lava = { name="Stylized flowing lava", author="Tarox" },
-	stylized_lava = { name="Stylized lava", author="Tarox" },
-	temporal_displacement = { name="Temporal displacement", author="DroppedBeat" },
-	train_tracks = { name="Train tracks", author="PixelMuncher" },
-	wires = { name="Wires", author="DroppedBeat" },
-}
-
-var MATERIAL_LIST_LONG : Array[String] = [
-	"roof_tiles", "polished_turquoise", "beach", "gingerbread",
-	"sewn_flesh", "ornamental", "gear_box", "floppy_disks",
-	"smaugs_treasure", "animated_fire", "arcane_compass", "wires",
-	"containers", "stylized_flowing_lava", "ancient_bricks",
-	"sealing_talismans", "temporal_displacement", "bookcase",
-	"gears_panel", "scarabs_on_hieroglyphs", "burger", "chained",
-	"hardwood_floor", "puddle", "cliff_rock", "manhole_cover",
-	"matrix_code_rain", "rosette", "kryptonite", "rainy_window",
-	"remnant", "ornamental_ceiling", "stylized_lava",
-	"train_tracks", "ancient_pedestal", "animated_radar",
-	"damaged_plaster", "fish_pond", "leather", "chesterfield",
-	"snowman", "ground_foliage"]
+var MATERIAL_LIST_SHORT : Array[String] = [ 
+	"cliff_rock", "cliff_rock", "cliff_rock", "cliff_rock", "rainy_window", "rosette", "manhole_cover", "gear_box", "ornamental_ceiling" ]
 
 
 func _ready():
-	material_names = MATERIAL_LIST_LONG
+	material_names = material_list.duplicate()
+	material_names.push_front(material_names.front())
+	material_names.push_front(material_names.front())
+	material_names.push_front(material_names.front())
+	if hide_material_names:
+		$Label.position.y = -1000
+		$LabelVertical.position.x = -1000
+	elif vertical:
+		$Label.position.y = -1000
+	else:
+		$LabelVertical.position.x = -1000
 	material_count = material_names.size()
+	
+	$Background.material_override.albedo_color = background_color
+	$Label.self_modulate = text_color
+	$LabelVertical.self_modulate = text_color
+	$LabelVertical/Author.self_modulate = text_color
 	
 	set_physics_process(false)
 	
@@ -99,8 +69,8 @@ func _ready():
 	tinymesh.size = Vector3(0.01, 0.01, 0.01)
 	
 	for m in material_names:
-		if m in MATERIALS:
-			var material = load("res://materials/"+m+".tres")
+		if m in material_descriptions:
+			var material = load(material_dir+"/"+m+".tres")
 			if material != null:
 				materials[m] = material
 				var mesh_instance : MeshInstance3D = MeshInstance3D.new()
@@ -135,15 +105,14 @@ func change_material():
 		$MeshPivot1/MeshPivot2/CirclePivot/Circle.visible = true
 		print(current_material)
 		if current_material & 1 == 0:
-			print("Rotate final 1")
 			$AnimationPlayer2.play("Rotate final 1", 0.2)
 			$MeshPivot1/MeshPivot2/Mesh2.visible = false
+			offset = 0.5*PI if vertical else 0.0
 		else:
-			print("Rotate final 2")
 			$AnimationPlayer2.play("Rotate final 2", 0.2)
 			$MeshPivot1/MeshPivot2/CirclePivot/Circle.rotation.z = PI
 			$MeshPivot1/MeshPivot2/Mesh1.visible = false
-			offset = PI
+			offset = 0.5*PI if vertical else PI
 			factor = -1.0
 		circle.material_override.set_shader_parameter("shader_time", -10.0)
 		var tween : Tween = get_tree().create_tween()
@@ -155,17 +124,25 @@ func change_material():
 		next_material = material_names[current_material]
 	current_material += 1
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	$MeshPivot1/MeshPivot2/CirclePivot.rotation.x = offset+factor*$MeshPivot1.rotation.x
 
 func update_label():
-	var material_name : String = next_material.get_basename()
-	if MATERIALS.has(material_name):
-		var material_desc = MATERIALS[material_name]
-		$Label.text = material_desc.name + " - " + material_desc.author
+	var material_id : String = next_material.get_basename()
+	if material_descriptions.has(material_id):
+		var material_desc : PackedStringArray = material_descriptions[material_id].split(",")
+		var material_name : String = material_desc[0]
+		var material_author : String = material_desc[1]
+		$Label.text = material_name + " - " + material_author
+		$LabelVertical.text = material_name
+		$LabelVertical/Author.text = material_author
 	else:
-		$Label.text = material_name
+		$Label.text = material_id
+		$LabelVertical.text = material_id
 		#print(material_name)
+
+func show_material_maker():
+	$ShowTitle.play("show_title_vertical" if vertical else "show_title_shrink")
 
 func end():
 	get_tree().quit()
